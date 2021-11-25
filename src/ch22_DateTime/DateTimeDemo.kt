37baf44ef54d360daf4f0c0d4c7937c0f632fdf5
Jaehi <damnyomoonwon1003@gmail.com>
2021-11-25 17:13:45 +0900

    22 DateTime

--- src/ch22_DateTime/DateTimeDemo.kt
+++ src/ch22_DateTime/DateTimeDemo.kt
@@ -1,2 +1,19 @@
 package ch22_DateTime
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun main() {
+    var current = LocalDateTime.now()
+    println("현재 시간 : $current")
+
+    var dateformatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")
+    println("현재 시간 : ${current.format(dateformatter)}")
+
+    println("날짜 : ${LocalDate.now()}")
+
+    println("3일 뒤 : ${current.plusDays(3)}")
+    println("3달 뒤 : ${current.plusMonths(3)}")
+    println("97일 뒤 : ${current.plusDays(97)}")
+}
